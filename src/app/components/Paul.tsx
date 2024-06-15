99da04af786aabbd17436c74681473bc9fcf57eb
Paul Stinchcombe <paul.stinchcombe@gmail.com>
2024-06-15 14:01:54 +0800

    Update to component

--- src/app/components/Paul.tsx
+++ src/app/components/Paul.tsx
@@ -12,7 +12,7 @@ export default function Paul() {
 		return () => {
 			console.log('Auth widget unmounted');
 		};
-	}, [window, openAuthWidget]);
+	}, [openAuthWidget]);
 
 	return (
 		<div className='font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
